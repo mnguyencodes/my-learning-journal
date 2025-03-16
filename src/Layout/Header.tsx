c89318d546f6a430295d26e5477cbec413493ab7
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-03-16 15:12:34 -0600

    Add Header component

---
+++ src/Layout/Header.tsx
@@ -0,0 +1,17 @@
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <div className="container">
+                    <a href="/index.html"><img src="/images/logo.png" alt="My Learning Journal logo" /></a>
+                    <a href="/index.html"><p>My Learning Journal</p></a>
+                </div>
+                <ul>
+                    <li><a href="/index.html">Home</a></li>
+                    <li><a href="/about-me.html">About Me</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
