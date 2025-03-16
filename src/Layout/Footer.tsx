47e9d6d78b3e2bd56aa7e99a9dae5c3b1f46e6d9
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-03-16 15:12:05 -0600

    Add Footer component

---
+++ src/Layout/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer>
+            <h2 className="m0">My Learning Journal</h2>
+            <p id="copyright" className="m0">Copyright ©2025</p>
+        </footer>
+    )
+}
+
