c7638ff95596c978b039028bcdfd2569394c39da
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-03-16 18:41:36 -0600

    Configure client-side routing
    - Implemented routing logic to ensure consistent rendering of the Header and Footer components across all pages.
    - Added a route for the home page.

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,19 @@ import {
   Routes,
   Route
 } from "react-router-dom"
+import Layout from "./components/Layout"
+import Home from "./pages/Home"
 
 function App() {
-  
-
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
