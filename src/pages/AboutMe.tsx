02cf6fd8160728ee390ecae43b43831c0f36e9d3
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-03-18 16:21:54 -0600

    Add new paragraph to AboutMe component

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -11,8 +11,11 @@ export default function AboutMe() {
             <section className="u-padding">
                 <article>
                     <h3>The Start of my Learning Journey</h3>
-                    <p>
+                    <p className="u-text-padding">
                         It all began in June of 2024 when I had an unexpected and fateful reunion with an old acquaintance. They had recently passed the CCNA certification exam and, after some friendly small talk, they encouraged me to pursue the same path. Despite my initial doubts—having no prior knowledge of networking—and grappling with a mid-life crisis about my career direction, I took a leap of faith. Without overthinking it, I embarked on my journey to earn my first certification: the CCNA.
+                    </p>
+                    <p className="u-text-padding">
+                        On November 16, 2024, I proudly passed the CCNA with flying colors. This accomplishment was a turning point for me--it made me realize that I'm capable of achieving anything I dedicate myself to. I spent the following week reflecting deeply on my next steps in life. Before long, I decided to return to programming, which is my foundation, as I hold a degree in computer science. My passion for video games initially reignited this spark, though game development isn't necessarily my goal. By November 23, 2024, I committed to pursuing a career as a full-stack developer.
                     </p>
                 </article>
             </section>
