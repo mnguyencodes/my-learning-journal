7096d09df1326eec45af9218c0de819a0dbe3bbd
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-03-19 13:07:05 -0600

    Refactor classes in MainBlog component
    - Refactored the classes in the MainBlog component to ensure consistent styles.

--- src/pages/MainBlog.tsx
+++ src/pages/MainBlog.tsx
@@ -1,3 +1,5 @@
+import "./MainBlog.css"
+
 export default function MainBlog() {
     return (
         <main className="u-padding">
@@ -7,13 +9,13 @@ export default function MainBlog() {
                 <p>
                     After two months of learning Python in the Python Programming MOOC 2024 course, I've jumped into the Frontend Developer Career Path on Scrimba to start my journey to become a fullstack developer.
                 </p>
-                <img className="img-main" src="/images/article-image-hero-large.jpg" />
+                <img className="u-img-margin" src="/images/article-image-hero-large.jpg" />
                 <section>
                     <h3>How I Got Started</h3>
-                    <p className="u-padding">
+                    <p className="u-text-padding">
                         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis id cumque dolorem, consequatur necessitatibus quaerat dolor repellendus nesciunt nihil, nulla a incidunt repellat est deserunt hic ipsum assumenda voluptatibus. Minus!
                     </p>
-                    <p className="u-padding">
+                    <p className="u-text-padding">
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. At laboriosam dolor earum odit harum. Sed sequi vitae, doloremque accusantium facere dignissimos. Non eos cupiditate asperiores suscipit corrupti! Saepe, mollitia at!
                     </p>
                 </section>
