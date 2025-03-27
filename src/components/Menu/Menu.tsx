aae70077fea41d269f2b74d395f52f86409dff37
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-03-27 08:31:36 -0600

    Add Menu component

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,21 @@
+import "./Menu.css"
+import {createContext} from "react"
+import {useToggle} from "../../hooks/useToggle"
+
+const MenuContext = createContext(undefined)
+export {MenuContext}
+
+export default function Menu({children, onOpen}) {
+    const [open, toggleOpen] = useToggle({
+        initialValue: false,
+        onToggle: onOpen
+    })
+    
+    return (
+        <MenuContext.Provider value={{open, toggleOpen}}>
+            <div className="menu">
+                {children}
+            </div>
+        </MenuContext.Provider>
+    )
+}
